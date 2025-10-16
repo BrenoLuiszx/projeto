4413c5606f84bafe3b839406f491e0a9855b5d03
BrenoLuiszx <robertaa.silvaa2017@gmail.com>
2025-10-16 01:33:22 -0300

    pritfinalboss

--- pizzariad-main/backend/src/main/java/com/learnly/model/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learnly.model.repository;
-
-import com.learnly.model.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
